1e3467c64908e396e14192afe77c3c22f6a21b97
drissakande <drissakande@gmail.com>
2023-04-10 11:36:57 -0400

    Modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+// findMatching function
+let drivers = [];
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // fuzzyMatch function
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  // matchName function
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
